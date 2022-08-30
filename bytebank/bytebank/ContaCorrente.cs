c9dcd30446f82a2391930a886d80435a15ade87d
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-30 08:52:49 -0300

    Criação de classes e objetos
    Criando primeira classe e objeto.

---
+++ bytebank/bytebank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numeroAgencia;
+        public string nomeAgencia;
+        public double saldo;
+
+    }
+}
---
+++ bytebank/bytebank/Program.cs
@@ -0,0 +1,18 @@
+﻿using bytebank;
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "10123-X";
+conta1.numeroAgencia = 23;
+conta1.nomeAgencia = "Agência Central";
+conta1.saldo = 100;
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número Agencia: " + conta1.numeroAgencia);
+Console.WriteLine("Nome Agencia: " + conta1.nomeAgencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+
+Console.ReadKey();
