51781283a88ca86edbf540395346cd65cb6c5209
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-30 16:59:56 -0300

    Criação de métodos

--- bytebank/bytebank/ContaCorrente.cs
+++ bytebank/bytebank/ContaCorrente.cs
@@ -7,6 +7,42 @@
         public int numeroAgencia;
         public string nomeAgencia;
         public double saldo;
-
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
     }
 }
--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -9,10 +9,18 @@ conta1.numeroAgencia = 23;
 conta1.nomeAgencia = "Agência Central";
 conta1.saldo = 100;
 
-Console.WriteLine("Titular: " + conta1.titular);
-Console.WriteLine("Conta: " + conta1.conta);
-Console.WriteLine("Número Agencia: " + conta1.numeroAgencia);
-Console.WriteLine("Nome Agencia: " + conta1.nomeAgencia);
-Console.WriteLine("Saldo: " + conta1.saldo);
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Ananda Silva";
+conta2.conta = "111999-X";
+conta2.numeroAgencia = 28;
+conta2.nomeAgencia = "Agência Central";
+conta2.saldo = 100;
+
+Console.WriteLine("Saldo da Amanda pré transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André pré transferência: " + conta1.saldo);
+bool transferencia = conta1.Transferir(50, conta2);
+Console.WriteLine("Transferência realizada com sucesso!");
+Console.WriteLine("Saldo da Amanda pós transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André pós transferência: " + conta1.saldo);
 
 Console.ReadKey();
